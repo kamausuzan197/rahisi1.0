af060bb28e601975d5e72d719e52051344855ba6
kamausuzan197 <kamaususan197@gmail.com>
2022-06-24 00:54:17 +0300

    validate form

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,12 +1,20 @@
 // grab the dom
 const getCertificatesButton = document.querySelector(".btn")
 let certs = document.querySelector(".certificates")
+const form = document.querySelector(".form")
+const Pname = document.querySelector("#name").value
+let email = document.querySelector("#email").value
+const certificate = document.querySelector("#Certificate-name").value
+const msg = document.querySelector("#msg").value
 
 
 // add an event listener to fetch the certificates when the button is clicked
 window.addEventListener('DOMContentLoaded', async (event) => {
     await getCertificatesButton.addEventListener("click", async()=>{
-        fetchData()
+        await fetchData()
+    })
+    await form.addEventListener("submit", () =>{
+        validateForm()
     })
     event.preventDefault()
 });
@@ -19,10 +27,10 @@ const fetchData = async() => {
         const html = data.map(content=>{
             return `
             <div>
-                <h2>${content.name}</h2>,
-                <h3>${content.title}</h3>,
+                <h2>Name: ${content.name}</h2>,
+                <h3>Type: ${content.title}</h3>,
                 <img src='${content.img}'</img>,
-                <button>Download</button>,
+                <button class='btn'>Download</button>,
 
             </div>
             `
@@ -30,4 +38,14 @@ const fetchData = async() => {
         console.log(html)
         certs.insertAdjacentHTML("afterbegin", html)
     })
+}
+// form validation
+const validateForm = () =>{
+    if(Pname === '' || email === '' || certificate === '' || msg === ''  ){
+        ralert("All fields are required")
+        return false
+    }else{
+        alert("form Submitted")
+        return true
+    }
 }
