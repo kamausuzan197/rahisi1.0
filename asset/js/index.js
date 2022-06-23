097725e088c3fce84af93afd4b9012b513b344a5
kamausuzan197 <kamaususan197@gmail.com>
2022-06-23 22:58:29 +0300

    asset/css/index.css

---
+++ asset/js/index.js
@@ -0,0 +1,33 @@
+// grab the dom
+const getCertificatesButton = document.querySelector(".btn")
+let certs = document.querySelector(".certificates")
+
+
+// add an event listener to fetch the certificates when the button is clicked
+window.addEventListener('DOMContentLoaded', (event) => {
+    getCertificatesButton.addEventListener("click", async()=>{
+        fetchData()
+    })
+
+});
+
+function fetchData(){
+    fetch("https://rahisiapi.herokuapp.com/rahisi/certs").then(response=>{
+        return response.json()
+    }).then(data=>{
+        console.log(data)
+        const html = data.map(content=>{
+            return `
+            <div>
+                <h2>${content.name}</h2>,
+                <h3>${content.title}</h3>,
+                <img src='${content.img}'</img>,
+                <button>Download</button>,
+
+            </div>
+            `
+        }).join("")
+        console.log(html)
+        certs.insertAdjacentHTML("afterbegin", html)
+    })
+}
